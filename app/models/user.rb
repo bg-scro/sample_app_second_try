d2a80aea5d2f32cb64a09e94851e95440429345f
Scrodaddy <bodom@three-wire.com>
2012-10-22 19:20:37 -0400

    (Mostly) Finished Exercises 7.6
    Broke my teeth on trying to set up RSpec tests for testing for presence of errors in the error message.  Attempted to use @user.errors.count on an invalid submission to test if any errors thrown.  Got a class error problem with this, so I have more research to do.  Most of the other exercises complete, though not pretty.
    
    Don't merge this with the master branch - all tests pass, but it's not clean...

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,5 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   validates :password_confirmation, presence: true
+  # note that line above removed per Ex 7.6, #3 discussion...
 end
